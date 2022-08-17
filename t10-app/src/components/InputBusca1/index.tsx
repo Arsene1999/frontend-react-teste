516dc3d73dd430de96d1ca7d2df55ac867f92ea9
João Victo Dantas <67969013+Arsene1999@users.noreply.github.com>
2022-08-17 15:17:37 -0300

    fix InputBusca 1 respo

--- t10-app/src/components/InputBusca1/index.tsx
+++ t10-app/src/components/InputBusca1/index.tsx
@@ -1,5 +1,5 @@
 import { SearchIcon } from "@chakra-ui/icons";
-import { InputGroup, Input, InputRightElement, Button, Text, Grid } from "@chakra-ui/react";
+import { InputGroup, Input, InputRightElement, Button, Text, Grid, Box, Flex } from "@chakra-ui/react";
 
 interface IInputBusca1{
     tamanhoFato: string,
@@ -14,36 +14,31 @@ export default function InputBusca1({
                                     }: IInputBusca1){
     
     return (
-        <Grid 
-            gap={6}
-            style={{   
-                display: 'flex', 
-                justifyContent: 'center',
-                flexWrap: 'wrap'      
-                }}
-            >
+        <>
             <Text fontSize='4xl' style={{paddingRight:'1rem'}}>Busca por Fatos</Text>
-            <InputGroup size='lg' maxWidth='600px'>
-                        
-                <Input
-                    type="number"
-                    pr='4.5rem'
-                    placeholder='Tamanho do fato'
-                    value={tamanhoFato}
-                    onChange={(event) => {setTamanhoFato(event.target.value)}}
-                />
-                        
-                <InputRightElement width='6.5rem'>
-                    <Button     	
-                        colorScheme='teal' 
-                        size='lg'
-                        onClick={handleFactSearch}
-                        leftIcon={<SearchIcon />}
-                        >
-                        Buscar  
-                    </Button>
-                </InputRightElement>
-            </InputGroup>   
-        </Grid>
+            <Flex justify='center' align='center' width='auto' p={5}>
+                <InputGroup size='lg' maxWidth='600px'>
+                            
+                    <Input
+                        type="number"
+                        pr='4.5rem'
+                        placeholder='Tamanho do fato'
+                        value={tamanhoFato}
+                        onChange={(event) => {setTamanhoFato(event.target.value)}}
+                    />
+                            
+                    <InputRightElement width='6.5rem'>
+                        <Button     	
+                            colorScheme='teal' 
+                            size='lg'
+                            onClick={handleFactSearch}
+                            leftIcon={<SearchIcon />}
+                            >
+                            Buscar  
+                        </Button>
+                    </InputRightElement>
+                </InputGroup> 
+            </Flex>  
+        </>
     );
 }
